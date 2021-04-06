49abd9a94c56fc82926aa6fa8a85a33c0fb47f73
cjaewon <jwn4492@outlook.com>
2021-04-06 15:26:44 +0900

    Add test cases

---
+++ meal_test.go
@@ -0,0 +1,45 @@
+package neisgo_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cjaewon/neisgo"
+)
+
+const (
+	apiKey = ""
+)
+
+func TestGetMeal(t *testing.T) {
+	cases := []struct {
+		year  int
+		month time.Month
+		len   int
+	}{
+		{
+			year:  2021,
+			month: 2,
+			len:   28,
+		},
+		{
+			year:  2021,
+			month: 1,
+			len:   31,
+		},
+	}
+
+	neis := neisgo.New(apiKey)
+	neis.Set("C10", "7150144")
+
+	for i, c := range cases {
+		meals, err := neis.GetMeal(c.year, c.month)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(meals) != c.len {
+			t.Fatalf("catched unexpected meals length, case %d", i)
+		}
+	}
+}
